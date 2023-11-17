ab371385f1cd97a28a6be496865187f7741aa055
Agustincito <agus@poap.io>
2023-11-17 23:47:48 +0300

    Add initial UI

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,27 @@
-import Image from 'next/image'
+import { ThemeProvider } from '@mui/material/styles';
+import {Box, Container, InputAdornment, TextField, Typography} from "@mui/material";
+import Header from "@/app/components/Header/header";
+import {AccountCircle} from "@mui/icons-material";
+import {FormControl} from "@mui/base";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      SCI
-    </main>
+      <>
+      <Header />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white text-black">
+            <Container>
+                <Box>
+                    <Typography variant="h2">SCI</Typography>
+                    <FormControl>
+                    <TextField label="Outlined" variant="outlined" >
+                        <InputAdornment position="end">
+                            <AccountCircle />
+                        </InputAdornment>
+                    </TextField>
+                    </FormControl>
+                </Box>
+            </Container>
+        </main>
+      </>
   )
 }
