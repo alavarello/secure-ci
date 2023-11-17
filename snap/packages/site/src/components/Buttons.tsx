b7dfb816e36182bcffbefaa871bae134dfd63bde
franicoETHGlobal <nicolas_d.l@hotmail.com>
2023-11-17 17:45:47 -0300

    Add snap scaffold

---
+++ snap/packages/site/src/components/Buttons.tsx
@@ -0,0 +1,159 @@
+import { ComponentProps } from 'react';
+import styled from 'styled-components';
+
+import { ReactComponent as FlaskFox } from '../assets/flask_fox.svg';
+import { MetamaskState } from '../hooks';
+import { shouldDisplayReconnectButton } from '../utils';
+
+const Link = styled.a`
+  display: flex;
+  align-self: flex-start;
+  align-items: center;
+  justify-content: center;
+  font-size: ${(props) => props.theme.fontSizes.small};
+  border-radius: ${(props) => props.theme.radii.button};
+  border: 1px solid ${(props) => props.theme.colors.background.inverse};
+  background-color: ${(props) => props.theme.colors.background.inverse};
+  color: ${(props) => props.theme.colors.text.inverse};
+  text-decoration: none;
+  font-weight: bold;
+  padding: 1rem;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: transparent;
+    border: 1px solid ${(props) => props.theme.colors.background.inverse};
+    color: ${(props) => props.theme.colors.text.default};
+  }
+
+  ${({ theme }) => theme.mediaQueries.small} {
+    width: 100%;
+    box-sizing: border-box;
+  }
+`;
+
+const Button = styled.button`
+  display: flex;
+  align-self: flex-start;
+  align-items: center;
+  justify-content: center;
+
+  ${({ theme }) => theme.mediaQueries.small} {
+    width: 100%;
+  }
+`;
+
+const ButtonForInstall = styled.button`
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 1rem 2rem;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  border-radius: 20px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  height: 100%;
+  &:hover {
+    background-color: #1e075f;
+    color: #ffffff;
+  }
+  &:disabled {
+    background-color: #f2e3ff;
+    cursor: not-allowed;
+    color: #000000;
+  }
+`;
+
+const ButtonText = styled.span`
+  margin-left: 1rem;
+`;
+
+const ConnectedContainer = styled.div`
+  display: flex;
+  align-self: flex-start;
+  align-items: center;
+  justify-content: center;
+  font-size: ${(props) => props.theme.fontSizes.small};
+  border-radius: ${(props) => props.theme.radii.button};
+  border: 1px solid ${(props) => props.theme.colors.background.inverse};
+  background-color: ${(props) => props.theme.colors.background.inverse};
+  color: ${(props) => props.theme.colors.text.inverse};
+  font-weight: bold;
+  padding: 1.2rem;
+`;
+
+const ConnectedIndicator = styled.div`
+  content: ' ';
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: green;
+`;
+
+export const InstallFlaskButton = () => (
+  <Link href="https://metamask.io/flask/" target="_blank">
+    <FlaskFox />
+    <ButtonText>Install MetaMask Flask</ButtonText>
+  </Link>
+);
+
+export const ConnectButton = (props: ComponentProps<typeof Button>) => {
+  return (
+    <Button {...props}>
+      <FlaskFox />
+      <ButtonText>Connect</ButtonText>
+    </Button>
+  );
+};
+
+export const InstallButton = (props: ComponentProps<typeof Button>) => {
+  return (
+    <ButtonForInstall {...props}>
+      <FlaskFox />
+      <ButtonText>
+        Install <i>{props.snapsNpmName}</i>{' '}
+      </ButtonText>
+    </ButtonForInstall>
+  );
+};
+export const ReconnectButton = (props: ComponentProps<typeof Button>) => {
+  return (
+    <Button {...props}>
+      <FlaskFox />
+      <ButtonText>Reconnect</ButtonText>
+    </Button>
+  );
+};
+
+export const SendHelloButton = (props: ComponentProps<typeof Button>) => {
+  return <Button {...props}>Send message</Button>;
+};
+
+export const HeaderButtons = ({
+  state,
+  onConnectClick,
+}: {
+  state: MetamaskState;
+  onConnectClick(): unknown;
+}) => {
+  if (!state.isFlask && !state.installedSnap) {
+    return <InstallFlaskButton />;
+  }
+
+  if (!state.installedSnap) {
+    return <ConnectButton onClick={onConnectClick} />;
+  }
+
+  if (shouldDisplayReconnectButton(state.installedSnap)) {
+    return <ReconnectButton onClick={onConnectClick} />;
+  }
+
+  return (
+    <ConnectedContainer>
+      <ConnectedIndicator />
+      <ButtonText>Connected</ButtonText>
+    </ConnectedContainer>
+  );
+};
