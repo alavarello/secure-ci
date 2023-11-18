31105556f03adbc9dc6be945218087fdf49454d8
Agustin Vazquez <agustin@poap.io>
2023-11-18 22:42:28 +0300

    Update index.tsx

--- app/pages/swap/index.tsx
+++ app/pages/swap/index.tsx
@@ -7,6 +7,8 @@ import {useState} from "react";
 import {useConnectedAddress} from "../../hooks/useConnectedAddress";
 import {useChainId} from "../../hooks/useChainId";
 import {useRouter} from "next/router";
+import {useQuery} from "react-query";
+import {getDomainsByContractAddress, getDomainWhitelistedAddresses} from "../../queries/domains";
 const cowParams: CowSwapWidgetParams = {
     "appCode": "secureCI COWSwap Integration",
     "width": "420px",
@@ -28,17 +30,25 @@ const Swap: NextPage = () => {
     const router = useRouter();
     const [contractAddress, setContractAddress] = useState<string>("");
     const provider = useCowswapProvider({setContractAddress});
-    const { address } = useConnectedAddress();
+    const domain = 'secureci.xyz';
     const { chainId: originalChainId } = useChainId();
     const supportedNetworks = [1, 5, 100] // 1 (Mainnet), 5 (Goerli), 100 (Gnosis)
     const isSupportedNetwork = supportedNetworks.includes(originalChainId);
 
+    const { data: whiteListedDomains, isLoading } = useQuery(
+        ['getDomainsByContract', contractAddress],
+        () => getDomainsByContractAddress(contractAddress)
+    )
+
+    const isVerified = !isLoading && contractAddress && whiteListedDomains && whiteListedDomains.domains.some((d) => d.id === domain);
     return (
         <div className={styles.main}>
             {!isSupportedNetwork && <h2 className={styles.unsupported}>Unsupported network <br /> (Please use Gnosis, Goerli or Mainnet)</h2>}
             {isSupportedNetwork &&
                 <>
                     <h2>Pool Smart Contracts verification by <strong>secureCI</strong></h2>
+                    {contractAddress && isLoading && <h3>Fetching secureCI subgraph for {contractAddress}</h3>}
+                    {contractAddress && !isLoading && <h3> {contractAddress} is {!isVerified && <strong>not</strong>} verified for {domain}</h3>}
                     <p>Domain: {domain}</p>
                     <CowSwapWidget params={{...cowParams, chainId: originalChainId, provider}} provider={provider} />
                 </>
