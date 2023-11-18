8064dedc9fd1df6d15e97d42714f402c4888739f
franicoETHGlobal <nicolas_d.l@hotmail.com>
2023-11-18 11:55:40 -0300

    Add AES to snaps

--- snaps/packages/snap/src/index.ts
+++ snaps/packages/snap/src/index.ts
@@ -1,5 +1,103 @@
-import type { OnRpcRequestHandler, OnTransactionHandler } from '@metamask/snaps-types';
-import { copyable, divider, heading, image, panel, text } from '@metamask/snaps-ui';
+import type { Json, OnRpcRequestHandler, OnTransactionHandler } from '@metamask/snaps-types';
+import { copyable, divider, heading, panel, text } from '@metamask/snaps-ui';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BigNumberish, toBeHex } from 'ethers';
+
+export const reportContractSchema = {
+  // Real
+  // uid: '0xb3e08a4b4e4d5f630f5a99978ad145cbe9eab4d9056764d5ae1dc7affcbc9649',
+
+  // Fake
+  uid: '0x83afd9a91bb9fc6178eccaac3ada2b308e841c4734e604ac6ee05baba196ce01',
+};
+
+export const reportDomainSchema = {
+  // Real
+  // uid: '0xeb484fecad7933bf4a88ee7f5308f167e5bdddfa6a751f618da49cd9497509af',
+
+  // Fake
+  uid: '0x6e5d0825f511b78440f9ff4e83bbaf4afeb51458bec3ca781612f2c944ef3d7f',
+};
+
+const EAS_SEPOLIA_GRAPH_QL = 'https://sepolia.easscan.org/graphql';
+
+export async function getReportsByContract(chainId: BigNumberish, contractAddress: string | number | boolean | Json[] | { [prop: string]: Json; } | null | undefined) {
+  const r = await queryEASGraphQl(`
+  query ReportsByContract {
+    aggregateAttestation(where: {
+      schemaId: {
+        equals: "${reportContractSchema.uid}"
+      }
+      AND: [
+        {
+          decodedDataJson: {
+            contains: "{\"name\":\"chainId\",\"type\":\"uint256\",\"value\":{\"type\":\"BigNumber\",\"hex\":\"${toBeHex(chainId)}\"}}"
+          }
+        },
+        {
+          decodedDataJson: {
+            contains: "{\"name\":\"contractAddress\",\"type\":\"address\",\"value\":\"${contractAddress}\"}"
+          }
+        }
+      ]
+    }) {
+      _count {
+        id
+      }
+    }
+  }
+  `)
+  if (!r) {
+    return 0;
+  }
+  return r.aggregateAttestation._count.id;
+}
+
+export async function getReportsByDomainName(domainName: string | undefined) {
+  const r = await queryEASGraphQl(`
+  query ReportsByDomain {
+    aggregateAttestation(where: {
+      schemaId: {
+        equals: "${reportDomainSchema.uid}"
+      }
+      decodedDataJson: {
+        contains: "{\"name\":\"domainName\",\"type\":\"string\",\"value\":\"${domainName}\"}"
+      }
+    }) {
+      _count {
+        id
+      }
+    }
+  }
+  `)
+  if (!r) {
+    return 0;
+  }
+  return r.aggregateAttestation._count.id;
+}
+
+async function queryEASGraphQl(query: string) {
+  const r = await fetch(EAS_SEPOLIA_GRAPH_QL, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+    })
+  })
+
+  if (r.status !== 200) {
+    return null;
+  }
+
+  const b = await r.json()
+
+  console.debug('eas graphql response', b)
+
+  return b.data;
+}
+
 
 /**
  * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
@@ -35,20 +133,29 @@ export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
 
 export const onTransaction: OnTransactionHandler = async ({
   transactionOrigin,
+  transaction,
   chainId,
 }) => {
+  const domain = transactionOrigin?.split('//').at(1);
+  const chainNumber = chainId.split(':').at(1)
+
+  const [reportsByContract, reportsByDomainName] = await Promise.all([
+    getReportsByContract(chainNumber as unknown as BigNumberish, transaction.to),
+    getReportsByDomainName(domain)
+  ])
+
   return {
     content: panel([
       heading('SCI Verification'),
-      text(`Domain:, **${transactionOrigin}**!`),
-      text(`On chain ${chainId}`),
+      text(`Origin: **${transactionOrigin}**!`),
+      text(`Chain ${chainId}`),
       divider(),
       text('SCI Flags:'),
-      text(`Domain reports ${0}`),
-      text(`Contract reports ${0}`),
+      text(`Domain reports ${reportsByDomainName}`),
+      text(`Contract reports ${reportsByContract}`),
       divider(),
       text('Additional info can be found at:'),
-      copyable(`_https://secureci.xyz/domains/${transactionOrigin?.split('//').at(1)}_`)
+      copyable(`https://secureci.xyz/domains/${domain}`)
     ]),
   };
 };
