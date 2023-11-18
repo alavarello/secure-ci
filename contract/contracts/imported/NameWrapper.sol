4a58ed6b4a01cc1f3e4137368da3e0e9fc1040fc
Agustin Vazquez <agustin@poap.io>
2023-11-18 19:50:50 +0300

    descomentado

--- contract/contracts/imported/NameWrapper.sol
+++ contract/contracts/imported/NameWrapper.sol
@@ -1,2 +1,2 @@
-//pragma solidity *;
-//import '@ensdomains/ens-contracts/contracts/wrapper/NameWrapper.sol';
+pragma solidity *;
+import "@ensdomains/ens-contracts/contracts/wrapper/NameWrapper.sol";
