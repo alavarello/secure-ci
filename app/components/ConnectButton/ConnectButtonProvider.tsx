e3cacf5cde522b159bb3cc51686d906665f77590
Agustincito <agus@poap.io>
2023-11-19 00:09:56 +0300

    Add ConnectButtonProvider

---
+++ app/components/ConnectButton/ConnectButtonProvider.tsx
@@ -0,0 +1,39 @@
+import {configureChains, createConfig, mainnet, WagmiConfig} from "wagmi";
+import { publicProvider } from 'wagmi/providers/public';
+import {getDefaultWallets, RainbowKitProvider} from "@rainbow-me/rainbowkit";
+import '@rainbow-me/rainbowkit/styles.css';
+import {hardhat} from "viem/chains";
+import {goerli, sepolia} from "wagmi/chains";
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import {ReactNode} from "react";
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+    [
+        sepolia,
+        goerli,
+        hardhat,
+    ],
+    [publicProvider(), alchemyProvider({ apiKey: 'SIfRQTz5lQuN8zKVhiS7pX4w2OR8q4Jo' })]
+);
+const { connectors } = getDefaultWallets({
+    appName: 'SecureCI',
+    projectId: 'dc226665666de6862c10b8f9c5caf7af',
+    chains,
+});
+
+const wagmiConfig = createConfig({
+    autoConnect: true,
+    connectors,
+    publicClient,
+    webSocketPublicClient,
+});
+
+function ConnectButtonProvider({children}: {children: ReactNode}) {
+    return <WagmiConfig config={wagmiConfig}>
+        <RainbowKitProvider chains={chains}>
+            {children}
+        </RainbowKitProvider>
+    </WagmiConfig>
+}
+
+export default ConnectButtonProvider;
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,17 +1,7 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import CssBaseline from '@mui/material/CssBaseline';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-    goerli,
-    mainnet,
-    hardhat,
-    sepolia,
-} from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { EASProvider } from '../stores/eas';
@@ -19,29 +9,9 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import { useState } from 'react';
 import ModalContextProvider from '../components/Modal/Modal.provider';
 import {StyledEngineProvider} from "@mui/material";
+import ConnectButtonProvider from "../components/ConnectButton/ConnectButtonProvider";
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [
-    mainnet,
-    sepolia,
-    goerli,
-    hardhat,
-  ],
-  [publicProvider(), alchemyProvider({ apiKey: 'SIfRQTz5lQuN8zKVhiS7pX4w2OR8q4Jo' })]
-);
 
-const { connectors } = getDefaultWallets({
-  appName: 'SecureCI',
-  projectId: 'dc226665666de6862c10b8f9c5caf7af',
-  chains,
-});
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-  webSocketPublicClient,
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
@@ -58,8 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <WagmiConfig config={wagmiConfig}>
-        <RainbowKitProvider chains={chains}>
+      <ConnectButtonProvider>
           <EASProvider>
             <StyledEngineProvider injectFirst>
                 <CssBaseline />
@@ -70,8 +39,7 @@ function MyApp({ Component, pageProps }: AppProps) {
                 </ModalContextProvider>
             </StyledEngineProvider>
           </EASProvider>
-        </RainbowKitProvider>
-      </WagmiConfig>
+      </ConnectButtonProvider>
     </QueryClientProvider>
   );
 }
