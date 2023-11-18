38ec5fb55841069f3dfb7657fa1edd24b981a44d
Santiago Echeverria <echeverriasi@gmail.com>
2023-11-18 16:51:05 +0300

    domain page edits

--- app/pages/domains/[domain].tsx
+++ app/pages/domains/[domain].tsx
@@ -8,6 +8,16 @@ import { useQuery } from 'react-query'
 import { useModalContext } from '../../components/Modal/Modal.provider';
 import Modal from '../../components/Modal/Modal';
 import WhitelistAddressesForm from '../../components/SubmitAddressesForm/WhitelistAddressesForm';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import '@fontsource/lexend/300.css';
+import '@fontsource/lexend/400.css';
+import '@fontsource/lexend/500.css';
+import '@fontsource/lexend/700.css';
 
 const CONNECTED_ADDRESS = '0xf032ecF3eDB10C103D9b99CEaa69E91be2D799f1'
 
@@ -19,16 +29,20 @@ const verifyDomain = (domain: string) => {
 }
 
 const Domain: NextPage = () => {
-  const router = useRouter()
-  const { openModal } = useModalContext()
-  const { domain } = router.query
-  const data = DUMMY_DOMAIN_DATA_FROM_SUBGRAPH.find(val => val.domain === domain)
-  const isVerified: boolean = data?.addresses != undefined && data?.addresses.length > 0
-  const { isLoading, refetch: verify } = useQuery(`verify-domain-${domain}`, () => verifyDomain(domain as string), {
+const router = useRouter()
+const { openModal } = useModalContext()
+const { domain } = router.query;
+const data = DUMMY_DOMAIN_DATA_FROM_SUBGRAPH.find(val => val.domain === domain)
+const isVerified: boolean = data?.addresses != undefined && data?.addresses.length > 0
+const { isLoading, refetch: verify } = useQuery(`verify-domain-${domain}`, () => verifyDomain(domain as string), {
     enabled: false,
-  })
+})
 
-  return (
+    function handleChange(event: SelectChangeEvent<any>, child: React.ReactNode): void {
+        throw new Error('Function not implemented.');
+    }
+
+return (
     <>
     <Modal>
         <WhitelistAddressesForm />
@@ -37,9 +51,33 @@ const Domain: NextPage = () => {
         <main className={styles.main}>
             {isVerified && data?.addresses ? 
                 <div className={styles.verifiedContainer}>
-                    <h2>{domain}</h2>
+                <div className={styles.addressContainer}>
+                <a href={`https://${domain}`} target='_blank'>
+                    <h2 className={styles.h3}>{domain}
+                    </h2>
+                    </a>
+                    </div>
                     <div className={styles.tableTitle}>
-                        <h4>Addresses</h4>
+                        <h4 className={styles.h4}>Addresses</h4>
+                        <Box sx={{ minWidth: 180 }}>
+        <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Blockchain</InputLabel>
+        <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value="Chain"
+        label="Chain"
+        onChange={handleChange}
+        >
+        <MenuItem value={10}>Mainnet</MenuItem>
+        <MenuItem value={20}>Arbitrum</MenuItem>
+        <MenuItem value={30}>Polygon</MenuItem>
+        <MenuItem value={40}>Sepolia</MenuItem>
+        <MenuItem value={50}>Goerli</MenuItem>
+        </Select>
+    </FormControl>
+    </Box>
+
                         <Button onClick={openModal}>Whitelist new addresses</Button>
                     </div>
                     <AddressesTable
@@ -57,7 +95,7 @@ const Domain: NextPage = () => {
         </main>
         </div>
     </>
-  );
+);
 };
 
 export default Domain;
