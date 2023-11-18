e1ae6b37866018896e94cc7c303caa6796b2c268
Juan M <jm@guide42.com>
2023-11-18 04:33:53 -0300

    Remove eas-resolver

--- eas-resolver/contracts/secure-ci/ISecureCIRegistry.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.19;
-
-interface ISecureCIRegistry {
-  function hasContract(uint256 chainId, address contractAddress) external returns (bool);
-}
